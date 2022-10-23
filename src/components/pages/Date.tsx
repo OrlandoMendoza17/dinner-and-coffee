175bca5ade8e75998bb741ab7b3956a8ec07fb3f
OrlandoMendoza17 <ommv.17@gmail.com>
2022-10-23 16:58:34 -0400

    Cambio de las fechas

--- src/components/pages/Date.tsx
+++ src/components/pages/Date.tsx
@@ -5,31 +5,31 @@ import ManyOptions from '../widgets/ManyOptions';
 const Date = () => {
 
   const dishes: OptionType[] = [
-    // {
-    //   id: 1,
-    //   name: "Sábado 22 Oct.",
-    //   value: "10/22/2022",
-    //   inputName: "date",
-    //   link: "/#Letter"
-    // },
     {
-      id: 2,
-      name: "Domingo 23 Oct.",
-      value: "10/23/2022",
-      inputName: "date",
-      link: "/#Letter"
-    },
-    {
-      id: 3,
+      id: 1,
       name: "Sábado 29 Oct.",
       value: "10/29/2022",
       inputName: "date",
       link: "/#Letter"
     },
     {
-      id: 4,
+      id: 2,
       name: "Domingo 30 Oct.",
       value: "10/30/2022",
+      inputName: "date",
+      link: "/#Letter"
+    },
+    {
+      id: 3,
+      name: "Sábado 5 Nov.",
+      value: "10/05/2022",
+      inputName: "date",
+      link: "/#Letter"
+    },
+    {
+      id: 4,
+      name: "Domingo 6 Nov.",
+      value: "10/06/2022",
       inputName: "date",
       link: "/#Letter"
     },
